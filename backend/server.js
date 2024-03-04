860ac1cdfd7ffac1142284b1099029d240a8daf1
Swasti008 <swastimohanty452@gmail.com>
2024-03-04 14:13:03 +0530

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const routes = require('./routes');
 const cors=require("cors");
 
 const app = express();
 
 app.use(cors())
+const routes = require('./routes');
 app.use(express.json());
 app.use('/crude-api', routes);
 
